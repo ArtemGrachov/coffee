8f09ac67a4478af8ec10533cc189e8eaaef017dd
Artem Grachov <grachov2195@gmail.com>
2017-12-06 11:52:06 +0200

    HTML/CSS/JS Gallery, partners sliders; svg in css (draft)

--- gulp/path/tasks.js
+++ gulp/path/tasks.js
@@ -1,12 +1,13 @@
 module.exports = [
-    './gulp/tasks/sass.js',
-    './gulp/tasks/pug.js',
-    './gulp/tasks/scripts.js',
-    './gulp/tasks/js-libs.js',
-    './gulp/tasks/css-libs.js',
-    './gulp/tasks/watch.js',
-    './gulp/tasks/fonts.js',
-    './gulp/tasks/img.js',
-    './gulp/tasks/browser-sync.js',
-    './gulp/tasks/clean.js'
-]
+    'sass.js',
+    'pug.js',
+    'scripts.js',
+    'js-libs.js',
+    'css-libs.js',
+    'watch.js',
+    'fonts.js',
+    'img.js',
+    'browser-sync.js',
+    'css-sprite.js',
+    'clean.js'
+].map(path => './gulp/tasks/' + path)
---
+++ gulp/tasks/css-sprite.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+    $.gulp.task('css-sprite', function () {
+        return $.gulp.src($.cfg.app + 'sprites/**/*.svg')
+            .pipe($.gp.svgmin({
+                js2svg: {
+                    pretty: true
+                }
+            }))
+            .pipe($.gp.svgCss({
+                fileName: '_generated',
+                addSize: false,
+                fileExt: 'scss'
+            }))
+            .pipe($.gulp.dest($.cfg.app + 'sass/sprites/'))
+    })
+}
