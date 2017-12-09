2ab8749cef2fac443ba11b1cd4762954c0512150
Artem Grachov <grachov2195@gmail.com>
2017-12-09 14:37:35 +0200

    JS Simple tabs animation

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,12 +1,12 @@
-const underlineSlide = (function () {
+const underlineSlide = (function() {
     return {
-        move: function (underline, toItem, animationSpeed) {
+        move: function(underline, toItem, animationSpeed) {
             underline.stop();
             underline.animate({
                 'left': (toItem.position().left + toItem.width() / 2) - underline.width() / 2
             }, animationSpeed)
         },
-        set: function (underline, toItem) {
+        set: function(underline, toItem) {
             underline.stop();
             underline.css({
                 'left': (toItem.position().left + toItem.width() / 2) - underline.width() / 2
@@ -14,3 +14,17 @@ const underlineSlide = (function () {
         }
     }
 })()
+
+const tabAnimation = function(newTab, oldTab, speed) {
+    newTab.stop();
+    oldTab.stop();
+    newTab.css({ 'display': 'none' });
+    oldTab.css({ 'display': 'block' });
+    oldTab.animate({
+        'opacity': 0
+    }, speed, () => {
+        newTab.css({ 'display': 'block', 'opacity': 0 });
+        oldTab.css({ 'display': 'none' });
+        newTab.animate({ 'opacity': 1 })
+    })
+}
