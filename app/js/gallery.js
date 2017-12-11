2e2d56f117ccc9dcc47f2f4bf1b0296c9c66681c
Artem Grachov <grachov2195@gmail.com>
2017-12-11 12:47:51 +0200

    HTML/CSS Header phone number, header and footer nav, gallery slider nav, focus fixes

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -1,14 +1,14 @@
-const gallerySlider = function() {
+const gallerySlider = function () {
     const gallerySlider = $('.gallery-slider-list');
     const activeNumber = $('#galleryActiveNumber'),
         totalSlides = $('#galleryTotal'),
-        setActiveNumber = function(number) {
+        setActiveNumber = function (number) {
             activeNumber.text(number);
         }
-    totalSlides.text(gallerySlider.children('li').length);
     gallerySlider
-        .on('init', function(e, slick) {
-            setActiveNumber(slick.slideCount)
+        .on('init', function (e, slick) {
+            setActiveNumber(slick.currentSlide + 1);
+            totalSlides.text(slick.slideCount);
         })
         .slick({
             adaptiveHeight: true,
@@ -20,7 +20,7 @@ const gallerySlider = function() {
             prevArrow: $('.gallery-slider').find('.gallery-slider__toggle_prev'),
             nextArrow: $('.gallery-slider').find('.gallery-slider__toggle_next')
         })
-        .on('beforeChange', function(e, slick) {
+        .on('beforeChange', function (e, slick) {
             setActiveNumber(slick.currentSlide + 1)
         })
 }
